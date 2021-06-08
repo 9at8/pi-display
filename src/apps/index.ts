fc91368b38742ac3da2cd37c0339ecba8610d0c0
Aditya Thakral <github@9at8.dev>
2021-06-08 14:44:13 -0400

    Only reload the weather widget

--- src/apps/index.ts
+++ src/apps/index.ts
@@ -2,6 +2,3 @@ import piHole from './pi-hole.js'
 import weather from './weather.js'
 
 export const apps = [weather, piHole]
-
-// reload the page every 60 mins
-setTimeout(() => location.reload(), 60 * 60 * 1000)
--- src/apps/weather.ts
+++ src/apps/weather.ts
@@ -1,7 +1,21 @@
 export default function render() {
   const div = document.createElement('div')
 
-  div.className = 'weather-app'
+  let widget = createWeatherWidget()
+  div.append(widget)
+
+  // reload the widget every hour
+  setInterval(() => {
+    div.removeChild(widget)
+    widget = createWeatherWidget()
+    div.append(widget)
+  }, 60 * 60 * 1000)
+
+  return div
+}
+
+function createWeatherWidget() {
+  const div = document.createElement('div')
 
   div.innerHTML = `
     <a
