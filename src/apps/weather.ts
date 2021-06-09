6ec29aac1fac3d43077ffe7972b12b8973fdea74
Aditya Thakral <github@9at8.dev>
2021-06-09 02:54:44 -0400

    Use eval instead of script tag

--- src/apps/weather.ts
+++ src/apps/weather.ts
@@ -30,13 +30,20 @@ function createWeatherWidget() {
     </a>
   `
 
-  const script = document.createElement('script')
-
-  script.innerHTML = `
-    !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
-  `
-
-  div.append(script)
+  setTimeout(() => {
+    eval(`
+      !(function (d, s, id) {
+        let js,
+          fjs = d.getElementsByTagName(s)[0]
+        if (!d.getElementById(id)) {
+          js = d.createElement(s)
+          js.id = id
+          js.src = 'https://weatherwidget.io/js/widget.min.js'
+          fjs.parentNode.insertBefore(js, fjs)
+        }
+      })(document, 'script', 'weatherwidget-io-js')
+    `)
+  }, 100)
 
   return div
 }
