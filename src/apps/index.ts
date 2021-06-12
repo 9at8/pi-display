7dad7b8c5c5b556014e502b07c293bb637a385fc
Aditya Thakral <github@9at8.dev>
2021-06-12 12:44:37 -0400

    Add Calendar

--- src/apps/index.ts
+++ src/apps/index.ts
@@ -1,4 +1,5 @@
+import calendar from './calendar.js'
 import piHole from './pi-hole.js'
 import weather from './weather.js'
 
-export const apps = [weather, piHole]
+export const apps = [weather, calendar, piHole]
